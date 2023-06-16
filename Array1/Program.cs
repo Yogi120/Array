99fd88aae3f1d1878ba997c13b21b43f3002235e
Yogesh <me.yogesh.pal@gmail.com>
2023-06-16 22:02:37 +0530

    Array problem 1

---
+++ Array1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Array1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10];
+            int i;
+
+            for (i=0; i<10; i++)
+            {
+                Console.Write("Elements {0} - " + i);
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (i=0; i<10; i++)
+            {
+                Console.Write("All elements are " + a[i]);
+            }
+        }
+    }
+}
