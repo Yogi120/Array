b96787a07f259c67734d303fbaf282e9ba87f37e
Yogesh <me.yogesh.pal@gmail.com>
2023-06-16 22:05:52 +0530

    Array problem 2

---
+++ Array2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Array2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+           int[] arr = new int[100];
+
+            Console.WriteLine("Enter the number of elements to print");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("Elements ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("All elements are " + arr[i]);
+            }
+
+            for (i = n - 1; i >= 0; i--)
+            {
+                Console.WriteLine("In reverse " + arr[i]);
+            }
+        }
+    }
+}
