df198f52e1441ada1cdcd29b51ad7ad62f6180b8
Yogesh <me.yogesh.pal@gmail.com>
2023-06-16 22:08:58 +0530

    Array problem 4

---
+++ Array4/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Array4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int[] arr2 = new int[100]; 
+            int[] arr = new int[100];
+            Console.WriteLine("Enter the number of element to copied : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("Elements are ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (i = 0; i < n; i++)
+            {
+                arr2[i] = arr[i];
+            }
+
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("Original Array is " + arr[i]);
+            }
+
+
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("Copied Array is " + arr2[i]);
+            }
+        }
+    }
+}
